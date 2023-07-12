b7f7a22c8c6177ca52fef983d13347a413a826ee
veronicamontenegro <veronicanahir@hotmail.com>
2023-07-11 21:26:07 -0300

    actualicé simulador1

--- main.js
+++ main.js
@@ -13,33 +13,32 @@ function sumarSaldo(saldo, montoDeCarga){
     return saldo + montoDeCarga;
 }
 
-let totalDelSaldo = sumarSaldo(saldo, montoDeCarga)
+let totalDelSaldo = sumarSaldo(saldo, montoDeCarga);
 
 let nuevaRecarga = 0;
 
-let acc = 0;
+let acc = saldo;
 
 for(i= 0; i < 7; i +=1){
-    acc += sumarSaldo(saldo, montoDeCarga);
+    acc = sumarSaldo(acc, montoDeCarga);
     nuevaRecarga +=1;
     console.log("------- Recarga nº " + nuevaRecarga + " -------")
 
-    if(totalDelSaldo > MAX_EN_TARJETA){
+    if(acc > MAX_EN_TARJETA){
         console.log("Alcanzó el máximo de saldo en la tarjeta. No se puede realizar la recarga")
-    } else if(totalDelSaldo > 0 && totalDelSaldo < MAX_EN_TARJETA){
-        console.log("Su recarga ha sido exitosa. Su saldo actual es de: $" + totalDelSaldo)
+        break
+    } else if(acc > 0 && acc < MAX_EN_TARJETA){
+        console.log("Su recarga ha sido exitosa. Su saldo actual es de: $" + acc)
     } else{
-        console.log("La recarga fue exitosa. Su saldo actual es de: $" + totalDelSaldo + ". Le recordamos que el saldo mínimo en la SUBE es de $" + MIN_EN_TARJETA)
+        console.log("La recarga fue exitosa. Su saldo actual es de: $" + acc + ". Le recordamos que el saldo mínimo en la SUBE es de $" + MIN_EN_TARJETA)
     }
 
     let repetirRecarga = confirm("¿Desea hacer otra recarga?")
 
     if(!repetirRecarga){
-        break;
+        break
     } else {
-        // saldo = totalDelSaldo;
         montoDeCarga = Number(prompt("¿Cuánto desea cargar?"));
-        
     }
 }
 
